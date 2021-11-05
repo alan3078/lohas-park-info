442d2a4c43763a5a6d25c000679a3846db9807b9
Oscar <dyfinalm@gmail.com>
2021-11-05 13:46:10 +0800

    Update Page

--- src/components/next-train/next-train.tsx
+++ src/components/next-train/next-train.tsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Container } from 'react-bootstrap';
 import getNextTrainData from './next-train-service';
 import { NextTrainResponse, line } from '../../typings/NextTrainResponse';
 import { msToTime } from '../../helpers/msToTime';
 
 import './next-train.scss'
+import { setSyntheticLeadingComments } from 'typescript';
 
 const NextTrain: React.FunctionComponent = () => {
   const [nextTrainData, setNextTrainData] = useState([] as line[])
-  const timeStamp = new Date()
-
+  const [timeUpdate, setTimeUpdate] = useState({varOne:new Date()})
+  const [timeStamp, setTimeStamp] = useState({varTwo:new Date()})
+  const [time, setTimes] = useState({ num: 0 });
+  const counter = useRef(0);
   const download = () => {
     getNextTrainData().then((result: NextTrainResponse) => {
       const data: line[] = result.data['TKL-LHP'].DOWN
@@ -19,21 +22,37 @@ const NextTrain: React.FunctionComponent = () => {
     })
   }
 
+  useEffect(() => {
+    counter.current += 1;
+    const timer = setTimeout(() => setTimes({ num: time.num + 1 }), 1000)
+    setTimeStamp({varTwo:new Date()})
+    if (counter.current % 30 === 0){
+      download()
+      setTimeUpdate({varOne:new Date()})
+    }
+    return () => clearTimeout(timer);
+  }, [time])
 
   useEffect(() => {
     download()
+    setTimeUpdate({varOne:new Date()})
   }, [])
 
   return (
     <div className="center">
       <Container>
-        最後更新時間: {timeStamp.toLocaleTimeString()}
+        最後更新時間: {timeUpdate.varOne.toLocaleTimeString()}
         <br />
         MTR Next Train
         <br />
         {nextTrainData.length > 0 ? nextTrainData.map((trainData) => {
           return (
-            <li className="train-list" key={trainData.ttnt}>{trainData.time} Remaining Time: {msToTime(Date.parse(trainData.time) - timeStamp.getTime())}
+            <li className="train-list" key={trainData.ttnt}>{trainData.time} 
+              {(Date.parse(trainData.time) - timeStamp.varTwo.getTime()) < 0 ? 
+                <span><br></br>走左喇, 下架啦！</span>
+                : 
+                <span><br></br>Remaing Time: {msToTime(Date.parse(trainData.time) - timeStamp.varTwo.getTime())}</span>
+              }
             </li>
           )
         }) : "已經無車啦"}
--- src/helpers/msToTime.tsx
+++ src/helpers/msToTime.tsx
@@ -1,10 +1,14 @@
 export const msToTime = (s: number) => {
-    var ms = s % 1000;
-    s = (s - ms) / 1000;
-    var secs = s % 60;
-    s = (s - secs) / 60;
-    var mins = s % 60;
-    var hrs = (s - mins) / 60;
-
-    return hrs + ':' + mins + ':' + secs + '.' + ms;
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = (s - mins) / 60;
+  if (hrs !== 0){
+    return hrs + ':' + mins + ':' + secs;
+  }else {
+    return mins + ':' + secs;
   }
+  
+}
