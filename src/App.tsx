37d4577190f8cf2df3c380e0bfbd0ceec7f04e00
Alan <yiukaiyeung1990@gmail.com>
2021-11-05 17:48:53 +0800

    add eng and zh-hk version

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TopNavBar from './components/top-nav-bar/top-nav-bar'
 import NextTrain from './components/next-train/next-train'
+import './i18n'
 
 const App: React.FC<{}> = () => (
   <>
--- src/components/top-nav-bar/top-nav-bar.tsx
+++ src/components/top-nav-bar/top-nav-bar.tsx
@@ -1,24 +1,34 @@
 import React from 'react'
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Nav, Navbar, Container, Button } from 'react-bootstrap'
 import logo from '../../assets/logo.png'
 import './top-nav-bar.scss'
+import { useTranslation } from 'react-i18next'
+import changeLanguage from '../../helpers/changeLanguage'
+import { languages } from '../../i18n'
 
-const TopNavBar: React.FunctionComponent = () => (
-  <div>
-    <Navbar expand="lg" className="bg">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img src={logo} className="logo" alt="logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </div>
-)
+const TopNavBar: React.FunctionComponent = () => {
+  const { t, i18n } = useTranslation()
+  const { language: currentLanguage } = i18n
+  const displayLangs = languages.filter((c) => c !== currentLanguage)[0]
+
+  return (
+    <div>
+      <Navbar expand="lg" className="bg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} className="logo" alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">{t('common:nextTrain')}</Nav.Link>
+              <Button variant="outline-dark" onClick={() => changeLanguage(displayLangs)} >{t(`common:${displayLangs}`)}</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
 
 export default TopNavBar
