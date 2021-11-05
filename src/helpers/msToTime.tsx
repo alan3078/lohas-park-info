ee600198fc145f32a0806e56a60cc7c89a602b8b
Oscar <dyfinalm@gmail.com>
2021-11-05 16:35:21 +0800

    update page

--- src/helpers/msToTime.tsx
+++ src/helpers/msToTime.tsx
@@ -1,14 +1,16 @@
 export const msToTime = (s: number) => {
-  var ms = s % 1000;
+  const ms = s % 1000;
   s = (s - ms) / 1000;
-  var secs = s % 60;
+  const secs = s % 60;
   s = (s - secs) / 60;
-  var mins = s % 60;
-  var hrs = (s - mins) / 60;
+  const mins = s % 60;
+  const hrs = (s - mins) / 60;
+  const hrsString = hrs.toString()
+  const minsString = mins.toString()
+  const secsString = secs.toString()
   if (hrs !== 0){
-    return hrs + ':' + mins + ':' + secs;
+    return hrsString + ':' + minsString + ':' + secsString;
   }else {
-    return mins + ':' + secs;
+    return minsString + ':' + secsString;
   }
-  
 }
